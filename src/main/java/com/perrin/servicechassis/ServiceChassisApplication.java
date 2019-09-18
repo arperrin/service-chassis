e4434418816a538129fa53f01d6c525e8e2650e0
Andrew Perrin <andrew.perrin@cgi.com>
2019-09-18 17:27:33 -0400

    initial commit

---
+++ src/main/java/com/perrin/servicechassis/ServiceChassisApplication.java
@@ -0,0 +1,13 @@
+package com.perrin.servicechassis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceChassisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceChassisApplication.class, args);
+	}
+
+}
